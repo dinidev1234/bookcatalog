011c90998ffb4db96d2a5c2dc7fa742c93554ec0
Dinislam <aikenamuteshova@gmail.com>
2023-12-13 19:21:34 +0600

    First commit

---
+++ book/models.py
@@ -0,0 +1,64 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+
+
+class CustomUser(AbstractUser):
+
+    def __str__(self):
+        return self.email
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    genre = models.ForeignKey('Genre', on_delete=models.CASCADE)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    description = models.TextField(null=True, blank=True)
+    publication_date = models.DateField(auto_now_add=True)
+    average_rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return f'{self.title}, {self.author}'
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, related_name='reviews', on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Review by {self.user} for {self.book}"
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+class FavoriteBook(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} added {self.book} to favorites"
+
+    class Meta:
+        unique_together = ('user', 'book')
